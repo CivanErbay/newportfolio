bfb122a2cde80a38883d774ba4d4e5b340419356
Civan <Erbay.Civan.Extern@swm.de>
2023-08-10 15:01:40 +0200

    Finish footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Nav from "@/components/views/Nav.vue";
 import SelfDesc from "@/components/views/SelfDesc.vue";
+import ImpressumBox from "@/components/views/ImpressumBox.vue";
 </script>
 
 <template>
-  <div class="max-w-9xl font-code-latin  px-4 md:px-8 lg:px-12">
+  <div class="app max-w-9xl font-code-latin text-sky-200 px-4 md:px-8 lg:px-12">
     <header class="flex justify-between pt-9 md:items-center">
       <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
       <Nav />
@@ -12,6 +13,7 @@ import SelfDesc from "@/components/views/SelfDesc.vue";
     </header>
 
     <main></main>
+    <footer class="mt-auto"><ImpressumBox /></footer>
   </div>
 </template>
 
---
+++ src/components/views/ImpressumBox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-sky-200 flex justify-end">
+    <div class="flex flex-col items-end">
+      <img
+        alt="Civan Erbay"
+        src="@/assets/pixel_me.png"
+        width="125"
+        height="125"
+        class="-mr-4"
+      />
+
+      <div class="flex items-center text-xl pb-2 pt-2 md:pb-4 md:pt-3 gap-8">
+        <img alt="Github" src="@/assets/github.svg" width="30" height="30" />
+        <img alt="linkedIn" src="@/assets/insta.svg" width="30" height="30" />
+        <img alt="Insta" src="@/assets/linked.svg" width="30" height="30" />
+        <a href="">Impressum</a>
+      </div>
+    </div>
+    <div class="flex-col -mr-12">
+      <p class="text-xl rotate-90 -mt-14 -mr-4">info@civan.io</p>
+      <p class="text-6xl mr-2 rotate-90 mt-[6rem]">2023</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
